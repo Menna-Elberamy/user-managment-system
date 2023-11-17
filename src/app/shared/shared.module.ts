dee1285926814be77fd6c658a1a75bbc2f29f879
Menna elberamy <menna.elberamy@outlook.com>
2023-11-17 23:15:32 +0200

    init Header Component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { LoaderComponent } from './loader/loader.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    LoaderComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    HeaderComponent,
+    LoaderComponent
   ]
 })
 export class SharedModule { }
