c12de6c49972750ea216ba0cf2b401237b9ae19f
Menna elberamy <menna.elberamy@outlook.com>
2023-11-18 03:59:12 +0200

    implement profile module

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
+import { profileRoutingModule } from './profile-routing.module';
 
 
 
@@ -9,7 +10,8 @@ import { ProfileComponent } from './profile/profile.component';
     ProfileComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    profileRoutingModule
   ]
 })
 export class ProfileModule { }
